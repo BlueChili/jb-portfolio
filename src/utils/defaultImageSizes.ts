ff20a26638ef1fe3a5606ea6bb28c845c62dcfe6
German Cabarcas <gcc@unironical.com>
2024-04-09 11:12:39 -0500

    Add width and height extraction to defaultImageSizes helper function

--- src/utils/defaultImageSizes.ts
+++ src/utils/defaultImageSizes.ts
@@ -1,6 +1,10 @@
-export default function defaultImageSizes(image: { width: number }) {
+import type { ImageMetadata } from 'astro';
+
+export default function defaultImageSizes(image: ImageMetadata) {
         return {
-                widths: [ 410, 640, 896, image.width ],
+                width: image.width,
+                height: image.height,
+                widths: [410, 640, 896, image.width],
                 sizes: `(max-width: 425px) 410px, (max-width: 768px) 640px, (max-width: 1024px) 896px, ${image.width}px`,
         };
 }
