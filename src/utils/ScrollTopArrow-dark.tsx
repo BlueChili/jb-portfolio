eea42c448eea849ad1948592baf750bbbcb3147d
ni-jbaruc <jdbaruc@gmail.com>
2024-07-02 23:33:38 -0800

    updates

---
+++ src/utils/ScrollTopArrow-dark.tsx
@@ -0,0 +1,36 @@
+'client:only';
+
+import { useState, useEffect } from 'preact/hooks';
+import type { FC } from 'preact/compat'; 
+import '../styles/scrollTopArrow-dark.css';
+
+type ScrollTopArrowProps = {
+        showBelow?: number;
+};
+const ScrollTopArrow: FC<ScrollTopArrowProps> = ({ showBelow = 400 }) => {
+        const [showScroll, setShowScroll] = useState<boolean>(true);
+
+        useEffect(() => {
+                const handleScroll = () => {
+                        const scrolled = window.scrollY;
+                        setShowScroll(scrolled > showBelow);
+                };
+                window.addEventListener('scroll', handleScroll);
+                handleScroll(); // Initial check
+                
+                return () => window.removeEventListener('scroll', handleScroll);
+        }, [showBelow]);
+
+        const scrollTop = () => {
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+        };
+        return showScroll ? (
+                <div
+                        className='scrollTop'
+                        onClick={scrollTop}>
+                        Back To Overview
+                </div>
+        ) : null;
+};
+
+export { ScrollTopArrow };
